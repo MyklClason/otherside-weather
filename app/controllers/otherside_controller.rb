88e79cbfd4c3a380526caaf5b3911df5f2788aff
Mykl Clason <myklclason@gmail.com>
2016-02-19 04:15:01 +0000

    Add controller; update search spec

---
+++ app/controllers/otherside_controller.rb
@@ -0,0 +1,4 @@
+class OthersideController < ApplicationController
+  def index
+  end
+end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,8 +8,25 @@ RSpec.feature "Otherside weather search" do
 
   scenario "successful search: city, town" do
     query = "Charlotte, North Carolina"
+    cordinates_lat = "N 35° 13' 37''"
+    cordinates_long = "W 80° 50' 35''"
     perform_otherside_weather_search(query)
     expect(page).to have_content("Weather on the otherside of #{query}")
+    #expect(page).to have_content("Coodinates: ")
+    
+    # Expct Weather conditions:
+    # -Temp
+    # -Wind (speed/direction)
+    # -Coordinates
+    # -weather
+    
+    # City name to   GPS   lookup: http://www.geonames.org/export/web-services.html#findNearbyPlaceName
+    #    GPS    to  city   lookup: http://www.geonames.org/export/web-services.html#findNearbyPlaceName
+    #    GPS    to weather lookup: http://openweathermap.org/current#geo
+    #     ''       station lookup: http://www.geonames.org/export/JSON-webservices.html#findNearByWeatherJSON
+    # Going to need to do bidirectional fraction to DMS convertion
+    
+    # Expect distance to exact opposite (based on expected GPS coodinates)
   end
 
   scenario "Search is unsuccessful, display errors" do
@@ -25,7 +42,7 @@ RSpec.feature "Otherside weather search" do
   
   def perform_otherside_weather_search(query)
     visit "/"
-    fill_in "Search", with: query
+    fill_in "Otherside", with: query
   end
 
 end
