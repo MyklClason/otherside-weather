9716f885a62f175ffe86fa87f3bf47bdedc28ac6
Mykl Clason <myklclason@gmail.com>
2016-02-19 05:51:22 +0000

    Update layout and specs

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -5,11 +5,20 @@ RSpec.feature "Otherside weather search" do
 
   before do
   end
+  
+  
+
+  scenario "check page layout" do
+    visit "/"
+    expect(page).to have_field("Search") # Search field
+    expect(page).to have_content("Otherside Weather") # Branding
+    expect(page).to have_content("Weather from the otherside") # Slogan
+  end
 
   scenario "successful search: city, town" do
     query = "Charlotte, North Carolina"
-    cordinates_lat = "N 35° 13' 37''"
-    cordinates_long = "W 80° 50' 35''"
+    #cordinates_lat = "N 35° 13' 37''"
+    #cordinates_long = "W 80° 50' 35''"
     perform_otherside_weather_search(query)
     expect(page).to have_content("Weather on the otherside of #{query}")
     #expect(page).to have_content("Coodinates: ")
@@ -25,6 +34,8 @@ RSpec.feature "Otherside weather search" do
     #    GPS    to weather lookup: http://openweathermap.org/current#geo
     #     ''       station lookup: http://www.geonames.org/export/JSON-webservices.html#findNearByWeatherJSON
     # Going to need to do bidirectional fraction to DMS convertion
+    
+    #SearchHTML: <%=http://api.geonames.org/search?name=la%20crosse&featureCode=PPL&maxRows=10&username=ENV["GEONAME"] %>
     
     # Expect distance to exact opposite (based on expected GPS coodinates)
   end
@@ -42,7 +53,7 @@ RSpec.feature "Otherside weather search" do
   
   def perform_otherside_weather_search(query)
     visit "/"
-    fill_in "Otherside", with: query
+    fill_in "Search", with: query
   end
 
 end
